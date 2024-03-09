e9945f2b93c0dcffabb8ced75686da946433bf9f
Eusebia Timon <eusebiat@mi-h5.msk.21-school.ru>
2024-03-09 21:41:34 +0300

    ready setup for sber coding

---
+++ brew_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl -fsSL https://rawgit.com/kube/42homebrew/master/install.sh | zs
+reset
---
+++ vs_code_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# need to install syntax material dark level 1 beta theme
+rm -f ~/Library/Application Support/Code/User/settings.json
+mv settings.json ~/Library/Application\ Support/Code/User/.
+
